ed1d26b20097065ac7a5977fc79ab14da08c6253
kc <2318122493@qq.com>
2024-12-01 20:48:30 +0800

    feat:头部导航栏高亮

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import logoImage from "../assets/logo.png" // 替换为你的实验室 logo 图片路径
 import "../styles/header.css"
 
@@ -9,38 +9,35 @@ const Header = () => {
       <div className="container">
         <div className="left">
           <div className="logo">
-            <Link to="/" className="logo-link">
+            <NavLink to="/" className="logo-link">
               <img src={logoImage} alt="实验室官网 Logo" />
-            </Link>
+            </NavLink>
           </div>
-          <Link to="/" className="logo-link">
+          <NavLink to="/" className="logo-link">
           <div className='title'>西安科技大学校软件实验室</div>
-          </Link>
+          </NavLink>
         </div>
         <nav className="nav">
           <ul className="nav-list">
             <li className="nav-item">
-              <Link to="/about" className="nav-link">
+              <NavLink to="/about" className="nav-link">
                 校软概况
-              </Link>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <Link to="/contact" className="nav-link">
+              <NavLink to="/contact" className="nav-link">
                 招新
-              </Link>
+              </NavLink>
             </li>
-            {/* <li className="nav-item">
-              <Link to="/services" className="nav-link">服务</Link>
-            </li> */}
             <li className="nav-item">
-              <Link to="/projects" className="nav-link">
+              <NavLink to="/projects" className="nav-link">
                 竞赛与项目
-              </Link>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <Link to="/activity" className="nav-link">
+              <NavLink to="/activity" className="nav-link">
                 校软活动
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </nav>
